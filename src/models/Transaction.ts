0abb1f82d6c401e46ad4a374576834ee6198f619
Mattheus Einloft <mattheuseinloft@gmail.com>
2020-05-22 16:57:07 -0300

    create transaction migration and model

--- src/models/Transaction.ts
+++ src/models/Transaction.ts
@@ -1,16 +1,29 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity('transactions')
 class Transaction {
+  @PrimaryGeneratedColumn('uuid')
   id: string;
 
+  @Column()
   title: string;
 
+  @Column()
   type: 'income' | 'outcome';
 
+  @Column('float')
   value: number;
 
-  category_id: string;
-
+  @CreateDateColumn()
   created_at: Date;
 
+  @UpdateDateColumn()
   updated_at: Date;
 }
 
